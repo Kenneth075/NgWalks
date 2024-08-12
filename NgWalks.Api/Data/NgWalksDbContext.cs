fbdd359551c6d7ce694b044e60599511dfcdcda8
Kenneth Edoho <kennethtibele@gmail.com>
2024-08-12 11:14:14 +0100

    Add DbContext

---
+++ NgWalks.Api/Data/NgWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NgWalks.Api.Models.Domain;
+
+namespace NgWalks.Api.Data
+{
+    public class NgWalksDbContext : DbContext
+    {
+        public NgWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
