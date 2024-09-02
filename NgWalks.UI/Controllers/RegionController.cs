13ca24d335187af57e75d84ad33bde0e871ab18d
Kenneth Edoho <kennethtibele@gmail.com>
2024-09-02 13:42:36 +0100

    Commit all changes

---
+++ NgWalks.UI/Controllers/RegionController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NgWalks.UI.Models.Dto;
+
+namespace NgWalks.UI.Controllers
+{
+    public class RegionController : Controller
+    {
+        private readonly IHttpClientFactory httpClient;
+        private readonly IConfiguration configuration;
+
+        public RegionController(IHttpClientFactory httpClient, IConfiguration configuration)
+        {
+            this.httpClient = httpClient;
+            this.configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> res = new List<RegionDto>();
+            try
+            {
+                //Get region Api
+                var regionApiUrl = configuration.GetValue<string>("Region:RegionApi");
+                var client = httpClient.CreateClient();
+                var response = await client.GetAsync(regionApiUrl);
+
+                response.EnsureSuccessStatusCode();
+                res.AddRange(await response.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View(res);
+        }
+    }
+}
---
+++ NgWalks.UI/Models/Dto/RegionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NgWalks.UI.Models.Dto
+{
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+        public required string Code { get; set; }
+        public required string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
